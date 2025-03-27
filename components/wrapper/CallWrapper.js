4ae843aedad7bc7af735f6581bbfc6f9958066a6
sameer52718 <alisameer52718@gmail.com>
2025-03-27 15:07:33 +0500

    Add incoming call alert in CallWrapper component
    - Integrated Alert to notify users of incoming calls
    - Provided options to accept or decline the call
    - Updated navigation logic to handle call acceptance

--- components/wrapper/CallWrapper.js
+++ components/wrapper/CallWrapper.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import { Alert } from 'react-native'
 import AuthService from '../../services/AuthService'
 import { navigate } from '../../navigations/RootNavigation'
 
@@ -13,7 +14,20 @@ const CallWrapper = ({ children }) => {
 
         const unsubscribe = AuthService.listenUserId(user.id, (data) => {
             if (data?.currentCall) {
-                navigate('Call', { id: data.currentCall })
+                Alert.alert(
+                    "Incoming Call",
+                    "You have an incoming call. Do you want to accept it?",
+                    [
+                        {
+                            text: "Decline",
+                            style: "cancel"
+                        },
+                        {
+                            text: "Accept",
+                            onPress: () => navigate('Call', { id: data.currentCall })
+                        }
+                    ]
+                )
             }
         })
 
@@ -26,3 +40,4 @@ const CallWrapper = ({ children }) => {
 }
 
 export default CallWrapper
+
