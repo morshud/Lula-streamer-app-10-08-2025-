8f34d5efdb6375a971bf0735dc86ef38421ead32
YazdanShaikh <yazdanshaikh11@gmail.com>
2025-04-05 14:50:05 +0500

    Enhance CallWrapper component to manage incoming call state
    - Added useState to track the current call state
    - Updated the logic to show an alert for incoming calls only when the call changes
    - Improved state management to prevent unnecessary updates

--- components/wrapper/CallWrapper.js
+++ components/wrapper/CallWrapper.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Alert } from 'react-native'
 import AuthService from '../../services/AuthService'
@@ -6,6 +6,7 @@ import { navigate } from '../../navigations/RootNavigation'
 
 const CallWrapper = ({ children }) => {
     const { user } = useSelector((state) => state.auth)
+    const [currentCall, setCurrentCall] = useState(null)
 
     useEffect(() => {
         if (!user) {
@@ -13,22 +14,27 @@ const CallWrapper = ({ children }) => {
         }
 
         const unsubscribe = AuthService.listenUserId(user.id, (data) => {
-            if (data?.currentCall) {
-                Alert.alert(
-                    "Incoming Call",
-                    "You have an incoming call. Do you want to accept it?",
-                    [
+            if (!data?.currentCall) return
+
+            setCurrentCall((prevCall) => {
+                console.log(prevCall, prevCall !== data.currentCall)
+
+                if (prevCall !== data.currentCall) {
+                    Alert.alert('Incoming Call', 'You have an incoming call. Do you want to accept it?', [
                         {
-                            text: "Decline",
-                            style: "cancel"
+                            text: 'Decline',
+                            style: 'cancel',
                         },
                         {
-                            text: "Accept",
-                            onPress: () => navigate('Call', { id: data.currentCall })
-                        }
-                    ]
-                )
-            }
+                            text: 'Accept',
+                            onPress: () => navigate('Call', { id: data.currentCall }),
+                        },
+                    ])
+                    return data.currentCall // Update state only if call changes
+                }
+                return prevCall // Keep state unchanged if call is the same
+            })
+
         })
 
         return () => {
